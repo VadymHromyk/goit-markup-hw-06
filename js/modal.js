38e25b451497570ef795fd2657d8dd38da6ba608
VadymHromyk <hromuk.vadim@gmail.com>
2026-02-02 02:13:29 +0200

    add mobile version

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,21 @@
 (() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openModalBtn: document.querySelector("[data-modal-open]"),
+  document.addEventListener("click", (e) => {
+    // Додати атрибут data-modal-open="modal-1" на кнопку відкриття
+    const openBtn = e.target.closest("[data-modal-open]");
     // Додати атрибут data-modal-close на кнопку закриття
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    modal: document.querySelector("[data-modal]"),
-  };
+    const closeBtn = e.target.closest("[data-modal-close]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    if (openBtn) {
+      const modalId = openBtn.dataset.modalOpen;
+      // Додати атрибут data-modal="modal-1" на бекдроп модалки
+      const modal = document.querySelector(`[data-modal="${modalId}"]`);
+      modal?.classList.add("is-open");
+    }
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle("is-open");
-  }
+    if (closeBtn) {
+      const modal = closeBtn.closest("[data-modal]");
+      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+      modal?.classList.remove("is-open");
+    }
+  });
 })();
